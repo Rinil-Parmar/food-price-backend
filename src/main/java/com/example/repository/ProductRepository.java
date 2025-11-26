5be6414ce2139121002c58e2b88a3f986f47d00a
Rinil-Parmar <parmarrinila2002@gmail.com>
2025-11-25 23:48:16 -0500

    inverted index and page ranking.

--- src/main/java/com/example/repository/ProductRepository.java
+++ src/main/java/com/example/repository/ProductRepository.java
@@ -7,4 +7,7 @@ import java.util.List;
 
 public interface ProductRepository extends MongoRepository<Product, String> {
     List<Product> findByCategory(String category);
+
+    boolean existsByProductNameIgnoreCase(String productName);
+
 }
